bd8ad3057f8c0638239f4569d80f83245f3adaaf
Sham-punka <130845477+Sham-punka@users.noreply.github.com>
2023-06-05 15:34:20 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+
+@dataclass
+class TgBot:
+    token: str            # Токен для доступа к телеграм-боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    return Config(tg_bot=TgBot(token=os.getenv('BOT_TOKEN')))
